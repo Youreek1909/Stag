996d9f453ad2eb26a78bbbef8c4687328c705a7a
Youreek <youreek91@gmail.com>
2019-02-24 14:48:54 +0000

    some changes

--- js/script.js
+++ js/script.js
@@ -11,4 +11,17 @@ $(document).ready(function() {
   $("#myCarousel").carousel();
 
 
+  $('#myCarousel').on('slide.bs.carousel', function () {
+    if ($('#item1').hasClass('active')) {
+      $('#item01').toggleClass('checked').siblings().removeClass('checked');
+    } else if ($('#item2').hasClass('active')) {
+      $('#item02').toggleClass('checked').siblings().removeClass('checked');
+    } else if ($('#item3').hasClass('active')) {
+      $('#item03').toggleClass('checked').siblings().removeClass('checked');
+    } else if ($('#item4').hasClass('active')) {
+      $('#item04').toggleClass('checked').siblings().removeClass('checked');
+    }
+  });
+
+
 });
